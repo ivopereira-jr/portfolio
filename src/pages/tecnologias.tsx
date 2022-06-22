080a58195060c7196703c644b802a318c04ccf38
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-06-22 09:39:31 -0300

    :sparkles: Pagina tecnologias

---
+++ src/pages/tecnologias.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+
+import { Layout } from '../components/Layout';
+
+const tecnologias: NextPage = () => {
+  return (
+    <Layout title="Tecnologias | Portfólio Ivo Pereira">
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </Layout>
+  );
+};
+
+export default tecnologias;
