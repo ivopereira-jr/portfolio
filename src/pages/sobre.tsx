413a37b212cfe26f0d4782af98f3b029fe558f98
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-06-22 09:39:13 -0300

    :sparkles: Pagina sobre

---
+++ src/pages/sobre.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+
+import { Layout } from '../components/Layout';
+
+const sobre: NextPage = () => {
+  return (
+    <Layout title="Sobre | Portfólio Ivo Pereira">
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </Layout>
+  );
+};
+
+export default sobre;
