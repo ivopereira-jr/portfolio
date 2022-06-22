4e96ca9281ae890ab0840160c9a65d501389d7fc
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-06-22 09:39:46 -0300

    :sparkles: Pagina contato

---
+++ src/pages/contato.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+
+import { Layout } from '../components/Layout';
+
+const contato: NextPage = () => {
+  return (
+    <Layout title="Contato | Portfólio Ivo Pereira">
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </Layout>
+  );
+};
+
+export default contato;
