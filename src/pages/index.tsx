5b9e29b49d927ce0d81ff664941f10615dfc7fb5
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-06-22 09:38:52 -0300

    :sparkles: Pagina de inicio

---
+++ src/pages/index.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import type { NextPage } from 'next';
+
+import { Layout } from '../components/Layout';
+
+import styles from '../styles/home.module.scss';
+
+const Home: NextPage = () => {
+  return (
+    <Layout title="Inicio | Portfólio Ivo Pereira">
+      <div className={`flexAlignCenterSpacebetween ${styles.container}`}>
+        <div className={styles.description}>
+          <h1>
+            Olá &nbsp;
+            <Image
+              src="/icons/hand-waving.gif"
+              alt="gif de mão acenando"
+              width={28}
+              height={28}
+            />
+            , eu sou
+          </h1>
+          <strong>Ivo Pereira</strong>
+          <h2>Desenvolvedor Front-end.</h2>
+        </div>
+
+        <div className={styles.photo}>
+          <Image
+            src="/icons/coding.svg"
+            alt="Imagem de um homem sentado em uma cadeira em frente a uma mesa ao lado de um vaso com uma planta e a frente dele tem algumas telas"
+            width={400}
+            height={400}
+            layout="responsive"
+            priority
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
