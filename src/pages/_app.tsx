46492589a5608c648ec3e96ca221f851a9daf3ae
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-06-22 09:40:15 -0300

    :sparkles: _App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
+import { AnimatePresence } from 'framer-motion';
+
+import { Header } from '../components/Header';
+
+import '../styles/globals.scss';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Header />
+
+      <NextNProgress
+        color="#2d99ff"
+        startPosition={0.2}
+        stopDelayMs={200}
+        height={2}
+        showOnShallow={true}
+        options={{ showSpinner: false }}
+      />
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </>
+  );
+}
+
+export default App;
