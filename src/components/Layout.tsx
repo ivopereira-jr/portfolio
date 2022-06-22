53b18a958f60c05b0745fa7f8f9ce46f9ba3551e
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-06-22 09:37:23 -0300

    :sparkles: Criação componente layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import { ReactNode } from 'react';
+import { motion } from 'framer-motion';
+
+interface LayoutProps {
+  title: string;
+  children: ReactNode;
+}
+
+export function Layout({ title, children }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <motion.main
+        layout
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -10, opacity: 0 }}
+        transition={{ duration: 0.6 }}
+        className="container paddingContainerContent"
+      >
+        {children}
+      </motion.main>
+    </>
+  );
+}
